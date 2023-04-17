582dfa3141b5d0f3f6026fdc4455166ee42243c7
danielson <dadjocy21@students.desu.edu>
2023-04-17 15:59:08 -0400

    progress

--- src/main/java/com/github/curriculeon/BinarySearchTreePrinter.java
+++ src/main/java/com/github/curriculeon/BinarySearchTreePrinter.java
@@ -8,11 +8,45 @@ public class BinarySearchTreePrinter<Key extends Comparable<Key>, Value> {
     }
 
     private String printInOrder(BinarySearchTreeNode<Key, Value> node, StringBuilder sb) {
-        return null; // TODO
+        //while (bst.getRoot() )
+        while (node != null){
+            if (node.getLeft() != null){
+                node = node.getLeft();
+            }
+            else if (node.getRight() != null){
+                node = node.getRight();
+            }
+            else{
+                sb.append(node.getKey());
+                bst.delete(node.getKey());
+            }
+        }
+        return sb.toString();
+    }
+
+    public boolean ShouldAdd(BinarySearchTreeNode<Key, Value> node, StringBuilder sb){
+        String value = node.getKey().toString();
+        return !sb.toString().contains(value);
     }
 
     private String printPreOrder(BinarySearchTreeNode<Key, Value> node, StringBuilder sb) {
-        return null; // TODO
+        int limit = 0;
+        while (limit < bst.size()){
+            if (ShouldAdd(node, sb)) {// IF THE KEY IS NOT IN THE STRING
+                sb.append(node.getKey()).append("\n");
+                limit++;
+            }
+            if (node.getLeft() != null && ShouldAdd(node.getLeft(), sb)){//IF THERE IS A NEW VALUE TO THE LEFT
+                node = node.getLeft();
+            }
+            else if (node.getRight() != null && ShouldAdd(node.getRight(), sb)){//IF THERE IS A NEW VALUE TO THE RIGHT
+                node = node.getRight();
+            }
+            else{
+                node = node.Previous();
+            }
+        }
+        return sb.toString();
     }
 
     private String printPostOrder(BinarySearchTreeNode<Key, Value> node, StringBuilder sb) {
